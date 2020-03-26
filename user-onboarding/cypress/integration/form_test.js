7fa8863fba4da3bb9e3327ca7131e8ed506f19ec
Cole Wilkison <wilkison.cole@gmail.com>
2020-03-26 19:09:27 -0400

    cypress done

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,21 @@
+describe("Testing our volunteer form", function() {
+    beforeEach(function() {
+      cy.visit("http://localhost:3000/");
+    });
+    it("Add test to inputs and submit form", function() {
+      cy.get('input[name="name"]')
+        .type("Cole")
+        .should("have.value", "Cole");
+    cy.get('input[name="email"]')
+        .type("email@email.com")
+        .should("have.value", "email@email.com");
+    cy.get('input[name="password"]')
+        .type("password")
+        .should("have.value", "password");
+    cy.get('[type="checkbox"]')
+        .check()
+        .should("be.checked");
+    cy.get("button").click();
+        
+    });
+  });
